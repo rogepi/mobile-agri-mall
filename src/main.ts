ec5f65510489ccf2030bd99ad15aea9c0da8545f
rogepi <zzr1119@outlook.com>
2021-04-07 13:17:55 +0800

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Vant from 'vant'
 import './assets/style/vant.theme.less'
+import './assets/style/tailwind.css'
 
 const app = createApp(App)
 app.use(Vant)
