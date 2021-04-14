e108c959e07a5e81a8fa2db8b5c7677241bc5ff0
rogepi <zzr1119@outlook.com>
2021-04-14 17:07:51 +0800

    add home grid

---
+++ src/views/home/home-grid.vue
@@ -0,0 +1,66 @@
+<template>
+  <van-grid column-num="5" :border="false">
+    <van-grid-item v-for="(item, index) in data" :key="index">
+      <img class="h-10" :src="item.imgSrc" alt="grid.svg" />
+      <span class="text-xs">{{ item.title }}</span>
+    </van-grid-item>
+  </van-grid>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+  setup() {
+    interface DataProps {
+      imgSrc: string
+      title: string
+    }
+    const data: DataProps[] = reactive([
+      {
+        imgSrc: '/src/assets/grid-svgs/白菜.svg',
+        title: '白菜',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/白萝卜.svg',
+        title: '白萝卜',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/草莓.svg',
+        title: '草莓',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/红辣椒.svg',
+        title: '红辣椒',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/西瓜.svg',
+        title: '西瓜',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/香蕉.svg',
+        title: '香蕉',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/玉米.svg',
+        title: '玉米',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/樱桃.svg',
+        title: '樱桃',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/黄瓜.svg',
+        title: '黄瓜',
+      },
+      {
+        imgSrc: '/src/assets/grid-svgs/橘子.svg',
+        title: '橘子',
+      },
+    ])
+    return {
+      data,
+    }
+  },
+})
+</script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="bg-gray-50">
     <div class="bggreen h-40 bg-green-400">
-      <div class="object-top">
-        <home-header></home-header>
-        <van-search
-          shape="round"
-          background="#34D399 "
-          placeholder="请输入搜索关键词"
-        />
-        <home-swipe class="z-1"></home-swipe>
-      </div>
+      <home-header></home-header>
+      <van-search
+        shape="round"
+        background="#34D399 "
+        placeholder="请输入搜索关键词"
+      />
     </div>
-    <div class="h-60"></div>
+    <home-swipe class="-mt-16"></home-swipe>
+    <home-grid></home-grid>
   </div>
 </template>
 
 <script lang="ts">
 import HomeHeader from './home-header.vue'
 import HomeSwipe from './home-swipe.vue'
+import HomeGrid from './home-grid.vue'
 export default {
   components: {
     HomeHeader,
     HomeSwipe,
+    HomeGrid,
   },
   setup() {},
 }
